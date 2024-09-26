6e53d49e59f54c3c92c01a351d60f9ac1296e433
Isaac Samuel <isaacsamuel002@gmail.com>
2024-09-26 12:48:00 -0300

    atualização

--- CadastroDeContatos/Controllers/ContatoController.cs
+++ CadastroDeContatos/Controllers/ContatoController.cs
@@ -8,5 +8,21 @@ namespace CadastroDeContatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
-}
+}   
+       
