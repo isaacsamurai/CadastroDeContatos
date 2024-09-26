5678df9c21f95d7cc53fa051029cb0b7a517d9aa
Isaac Samuel <isaacsamuel002@gmail.com>
2024-09-26 09:55:55 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeContatos/Controllers/RedeSocial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeContatos.Controllers
+{
+    public class RedeSocial : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
